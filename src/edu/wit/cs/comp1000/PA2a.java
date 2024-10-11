c641efbba7bd22dac74e591a69fb35769e3990d8
williamangel <angelw@wit.edu>
2024-10-10 23:40:12 -0400

    Completed program all Junit tests passed java docs included

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,98 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+import java.util.Scanner;
+
+
+/**
+ * This program takes 5 numbers from the user and detects the number of positive and negative numbers takes the sum of all 
+ * then takes the averages respectivly 
+ * 
+ * @author William Angel
+ */
+
 public class PA2a {
 
-	// TODO: document this function
+	/**
+	 * This starts the program and outputs the sum of the positive, negative, and all numbers
+	 * Then calculates the averages respectively 
+	 * 
+	 * @param args comman-line arguments,ignored
+	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner input = new Scanner(System.in);
+	
+System.out.printf("Enter five whole numbers: ");
+
+int sumALL = 0;
+int sum_pos_num = 0;
+int sum_neg_num = 0;
+int sum_all_num = 0;
+int count_positive = 0;
+int count_negative = 0;
+	
+// Read 5 numbers from user and add them together 
+for(int i = 0; i < 5; i++) {
+	int num = input.nextInt();
+	sumALL += num;
+	
+// Detects if numbers are positive and counts the number of positive integers 
+	if (num > 0) {
+		sum_pos_num += num;
+		
+		count_positive++;
+		
+	}else { 
+		sum_neg_num += num;
+		count_negative++;
+		
+	}
+	
+}
+	
+
+//Calculate averages
+double avgALL = sumALL / 5.0;
+double avgPositive = (count_positive > 0) ? (sum_pos_num / (double) count_positive) : 0.0;
+double avgNegative = (count_negative > 0) ? (sum_neg_num / (double) count_negative) : 0.0;
+
+//output results to console
+
+if (count_positive == 1) {
+	System.out.println("The sum of the 1 positive number: " + sum_pos_num);
+}else {System.out.println("The sum of the " + count_positive + " positive numbers: " + sum_pos_num);
+
+}
+
+if (count_negative == 1) {
+    System.out.println("The sum of the 1 non-positive number: " + sum_neg_num);
+} else {
+    System.out.println("The sum of the " + count_negative + " non-positive numbers: " + sum_neg_num);
+}
+
+System.out.println("The sum of the 5 numbers: " + sumALL);
+
+if (count_positive == 1) {
+    System.out.printf("The average of the 1 positive number: %.2f%n", avgPositive);
+} else {
+    System.out.printf("The average of the %d positive numbers: %.2f%n", count_positive, avgPositive);
+}
+
+if (count_negative == 1) {
+    System.out.printf("The average of the 1 non-positive number: %.2f%n", avgNegative);
+} else {
+    System.out.printf("The average of the %d non-positive numbers: %.2f%n", count_negative, avgNegative);
+}
+
+
+System.out.printf("The average of the 5 numbers: %.2f%n",avgALL);
+
+
+
+
+	
+	
+
+		
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,58 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
-public class PA2b {
+import java.util.Scanner;
+import java.lang.Math;
 
-	// TODO: document this function
+/**
+ * Solution to second programming assignment solves for x using the quadratic equation taking the user inputed a b and c 
+ * 
+ * @author William Angel
+ */
+
+public class PA2b {
+/**
+ * this starts the program and reads the user input to calculate the discriminate and the roots
+ * @param args comman-lines arguments, ignored 
+ */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.printf("Enter a b c: ");
+		
+		double a = input.nextDouble();
+		double b = input.nextDouble();
+		double c = input.nextDouble();
+		
+		
+		double d = (b*b) - (4*a*c);
+		
+		
+		if (d < 0) {
+			System.out.println("Roots: imaginary");
+			
+		}
+		
+		else if (d == 0) {
+			
+			double root = (-b) / (2*a);
+			
+			System.out.printf("Root: %.2f%n", root);
+			
+		}
+		
+		
+		else {
+			
+			double root1 = (-b + Math.sqrt(d)) / (2*a);
+			double root2 = (-b - Math.sqrt(d)) / (2*a);
+			
+			double smallerroot = Math.min(root1, root2);
+			double largerroot = Math.max(root1, root2);
+			
+			System.out.printf("Roots: %.2f, %.2f%n", smallerroot, largerroot);
+			
+		}
 	}
 
 }
